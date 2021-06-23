33a440add5c0daec02ab07ccfbe7d65dff3c4478
NikitaPirate <spasepirat@gmail.com>
2021-06-23 14:22:06 +0400

    Message sender

--- src/message_sender.py
+++ src/message_sender.py
@@ -5,14 +5,14 @@ from tg.notice_sender import send_message
 import telegram
 
 if __name__ == '__main__':
-    users = session.query(User).all()
+    users = session.query(User).filter(User.id > 393).all()
     text = """
 ⚠️⚠️⚠️ Need your support ⚠️⚠️⚠️
 https://gitcoin.co/grants/2137/eth-gas-price-telegram-bot
 Please support the project. This is necessary to maintain and develop the service. Any participation, even 1 DAI, is important."""
     sended = 0
     deleted = 0
-    for user in users[394:]:
+    for user in users:
         try:
             send_message(user.tg_id, text)
             sended += 1
