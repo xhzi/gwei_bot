f76958b83c422ab5f656fdee75de760d1925e5bc
NikitaPirate <spasepirat@gmail.com>
2021-10-12 13:54:31 +0300

    Update GP_type enum

--- src/gasprice/gasPrice.py
+++ src/gasprice/gasPrice.py
@@ -7,10 +7,9 @@ requests.packages.urllib3.disable_warnings()
 
 
 class GP_type(enum.Enum):
-    fastest = 0
-    fast = 1
-    standard = 2
-    slow = 3
+    fast = 0
+    average = 1
+    slow = 2
 
 
 class GP:
